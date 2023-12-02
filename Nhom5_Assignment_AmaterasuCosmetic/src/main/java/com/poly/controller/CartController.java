6417be35a463a7eab95fd42b79c2a41aa86b3a3b
ACER <ACER@LAPTOP-27RAI7KA>
2023-12-02 15:51:58 +0700

    //Gio hang - CartItem

--- Nhom5_Assignment_AmaterasuCosmetic/src/main/java/com/poly/controller/CartController.java
+++ Nhom5_Assignment_AmaterasuCosmetic/src/main/java/com/poly/controller/CartController.java
@@ -1,15 +1,10 @@
 package com.poly.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.poly.dao.ProductDAO;

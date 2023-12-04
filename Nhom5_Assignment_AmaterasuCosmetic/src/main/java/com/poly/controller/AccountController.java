85d6f5ace9471021e6d7ac7479018515f7fbaf9f
Admin <Admin@Admin>
2023-12-05 04:58:21 +0700

    dang nhap 40%

--- Nhom5_Assignment_AmaterasuCosmetic/src/main/java/com/poly/controller/AccountController.java
+++ Nhom5_Assignment_AmaterasuCosmetic/src/main/java/com/poly/controller/AccountController.java
@@ -1,20 +1,56 @@
 package com.poly.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.poly.dao.AccountDao;
 import com.poly.entity.Account;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class AccountController {
+	
+	@Autowired
+	AccountDao aDao;
+	
+	
 
 	@GetMapping("/login")
-	public String login() {
+	public String login(Model model) {
+		model.addAttribute("checkpass", false);
 		return "User/login";
+	}
+	
+	@PostMapping("/login")
+	public String home(Model model, @RequestParam("username") String username, @RequestParam("password") String password, HttpServletRequest request) {
+		
+		Account account = aDao.findByUsername(username);
+		
+		if (account != null) {
+			if (account.getPassword().equals(password)) {
+				// Tạo một đối tượng session
+			    HttpSession session = request.getSession();
+
+			    // Thêm dữ liệu tên người dùng vào session
+			    session.setAttribute("username", username);
+				
+			    return "redirect:/home";
+			}
+			model.addAttribute("checkpass", true);
+			return "User/login";
+		} else {
+			model.addAttribute("checkpass", true);
+			return "User/login";
+		}
+
 	}
 
 	@GetMapping("/forgotPassword")

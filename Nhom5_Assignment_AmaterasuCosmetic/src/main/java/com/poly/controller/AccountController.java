99aff4b1e29cc17202b5d38e7899c46c26d9eb44
ACER <ACER@LAPTOP-27RAI7KA>
2023-12-05 15:55:02 +0700

    //huy

--- Nhom5_Assignment_AmaterasuCosmetic/src/main/java/com/poly/controller/AccountController.java
+++ Nhom5_Assignment_AmaterasuCosmetic/src/main/java/com/poly/controller/AccountController.java
@@ -33,7 +33,7 @@ public class AccountController {
 		return "User/changePassword";
 	}
 
-	//registerss
+	//registerssss
 	@PostMapping("/register")
 	public String register(@ModelAttribute Account account, Model model) {
 		
